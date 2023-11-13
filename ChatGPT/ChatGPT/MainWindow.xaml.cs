f2ab3be1c1f0afe0d52a59d3b40d15a19b4bf85e
GunnerTheEgg <123881845+GunnerTheCoder@users.noreply.github.com>
2023-11-12 20:11:31 -0500

    Add files via upload

---
+++ ChatGPT/ChatGPT/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        private int targetNumber;
+        private string targetWord;
+        private int num1, num2, mathAnswer;
+        private int attempts = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartNewGame();
+            StartNewWordGame();
+            StartNewMathGame();
+        }
+
+        private void StartNewGame()
+        {
+            Random random = new Random();
+            targetNumber = random.Next(1, 101);
+            attempts = 0;
+            resultText.Text = "";
+        }
+
+        private void StartNewWordGame()
+        {
+            string[] words = { "apple", "banana", "chocolate", "elephant", "programming" };
+            Random random = new Random();
+            targetWord = words[random.Next(0, words.Length)].ToLower();
+            wordGuessTextBox.Text = "";
+            wordResultText.Text = "";
+        }
+
+        private void StartNewMathGame()
+        {
+            Random random = new Random();
+            num1 = random.Next(1, 11);
+            num2 = random.Next(1, 11);
+            mathAnswer = num1 + num2;
+            mathAnswerTextBox.Text = "";
+            mathResultText.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int userGuess = int.Parse(guessTextBox.Text);
+                CheckGuess(userGuess);
+            }
+            catch (FormatException)
+            {
+                resultText.Text = "Please enter a valid number.";
+            }
+        }
+
+        private void WordButton_Click(object sender, RoutedEventArgs e)
+        {
+            string userGuess = wordGuessTextBox.Text.ToLower();
+            CheckWordGuess(userGuess);
+        }
+
+        private void MathButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int userAnswer = int.Parse(mathAnswerTextBox.Text);
+                CheckMathAnswer(userAnswer);
+            }
+            catch (FormatException)
+            {
+                mathResultText.Text = "Please enter a valid number.";
+            }
+        }
+
+        private void CheckGuess(int guess)
+        {
+            attempts++;
+
+            if (guess < targetNumber)
+            {
+                resultText.Text = $"Too low! Try again. Attempts: {attempts}";
+            }
+            else if (guess > targetNumber)
+            {
+                resultText.Text = $"Too high! Try again. Attempts: {attempts}";
+            }
+            else
+            {
+                resultText.Text = $"Congratulations! You guessed the number in {attempts} attempts.";
+                StartNewGame();
+            }
+        }
+
+        private void CheckWordGuess(string guess)
+        {
+            if (guess == targetWord)
+            {
+                wordResultText.Text = "Congratulations! You guessed the word.";
+                StartNewWordGame();
+            }
+            else
+            {
+                wordResultText.Text = "Incorrect. Try again.";
+            }
+        }
+
+        private void CheckMathAnswer(int answer)
+        {
+            if (answer == mathAnswer)
+            {
+                mathResultText.Text = "Correct! You solved the math problem.";
+                StartNewMathGame();
+            }
+            else
+            {
+                mathResultText.Text = "Incorrect. Try again.";
+            }
+        }
+    }
+}
---
+++ FileExplorerApp/FileExplorerApp/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FileExplorerApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDrives();
+        }
+
+        private void LoadDrives()
+        {
+            string[] drives = Directory.GetLogicalDrives();
+
+            foreach (string drive in drives)
+            {
+                Button button = CreateButton(drive);
+                button.Click += DriveButtonClick;
+                stackPanel.Children.Add(button);
+            }
+        }
+
+        private void DriveButtonClick(object sender, RoutedEventArgs e)
+        {
+            string drive = (string)((Button)sender).Tag;
+            LoadDirectories(drive);
+        }
+
+        private void LoadDirectories(string path)
+        {
+            try
+            {
+                stackPanel.Children.Clear();
+
+                // Made by Gunner Castle
+                Button backButton = CreateButton("⬅ Back");
+                backButton.Click += BackButtonClick;
+                stackPanel.Children.Add(backButton);
+
+                if (string.IsNullOrEmpty(path))
+                {
+                    locationBar.Text = "Drive Root";
+                }
+                else
+                {
+                    string[] directories = Directory.GetDirectories(path);
+                    foreach (string directory in directories)
+                    {
+                        Button button = CreateButton(directory);
+                        button.Click += DirectoryButtonClick;
+                        stackPanel.Children.Add(button);
+                    }
+
+                    string[] files = Directory.GetFiles(path);
+                    foreach (string file in files)
+                    {
+                        Button button = CreateButton(file);
+                        button.Click += DirectoryButtonClick; // Click event to handle both files and directories
+                        stackPanel.Children.Add(button);
+                    }
+
+                    locationBar.Text = path;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access to the directory is denied.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            string currentPath = locationBar.Text;
+            string parentPath = Directory.GetParent(currentPath)?.FullName;
+
+            if (!string.IsNullOrEmpty(parentPath))
+            {
+                LoadDirectories(parentPath);
+            }
+        }
+
+        private void DirectoryButtonClick(object sender, RoutedEventArgs e)
+        {
+            string path = (string)((Button)sender).Tag;
+
+            if (File.Exists(path))
+            {
+                // If the selected item is a file, open it
+                OpenFile(path);
+            }
+            else
+            {
+                // If the selected item is a directory, load its contents
+                LoadDirectories(path);
+            }
+        }
+
+        private void OpenFile(string filePath)
+        {
+            try
+            {
+                string extension = Path.GetExtension(filePath);
+
+                // Check the file extension and open accordingly
+                switch (extension.ToLower())
+                {
+                    case ".html":
+                    case ".htm":
+                        // Open HTML files in the default web browser
+                        Process.Start(filePath);
+                        break;
+
+                    case ".exe":
+                        // Run executable files with the default associated program
+                        Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
+                        break;
+
+                    // Add more cases for other file types as needed
+
+                    default:
+                        MessageBox.Show($"Unable to open file of type '{extension}'.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private Button CreateButton(string path)
+        {
+            string displayText = string.IsNullOrEmpty(path) ? "Drive Root" : Path.GetFileName(path);
+
+            return new Button
+            {
+                Content = displayText,
+                Tag = path,
+                Width = 200,
+                Height = 30,
+                Margin = new Thickness(5),
+            };
+        }
+    }
+}
